fca3a5c10bf243b58f03fa9b32bc2c35973283bf
refleks1a <145009608+refleks1a@users.noreply.github.com>
2023-11-28 23:29:56 +0400

    Remove django-secret-key from settings.py

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(#c@3=qa5b5@c&-^un&qk5cs5k0cn*69aari%v*nv)4p^-6@pz'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
